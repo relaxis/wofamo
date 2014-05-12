89567516cdb5c26ad4bb24bc08d95aa8d4d4174d
marcusorly <marcusorly@gmail.com>
2014-05-12 23:46:53 +0200

    intros splash again

--- wofamo/main.c
+++ wofamo/main.c
@@ -6,14 +6,14 @@
 
 int main()
 {
-    printf("        Welcome, LADIES...and....GENTLEMEN!");
-    printf("             to...the one and only...");
-    printf("******************************************************");
-    printf("*                                                    *");
-    prinft("*         $|$ THE WHEEL OF FORTUNE $|$               *");
-    printf("*               €|€ THE GAME €|€                     *");
-    printf("*                                                    *");
-    printf("******************************************************");
+    printf("        Welcome, LADIES...and....GENTLEMEN!\n");
+    printf("             to...the one and only...\n");
+    printf("******************************************************\n");
+    printf("*                                                    *\n");
+    printf("*         $|$ THE WHEEL OF FORTUNE $|$               *\n");
+    printf("*               €|€ THE GAME €|€                     *\n");
+    printf("*                                                    *\n");
+    printf("******************************************************\n");
 
     printf("This is a random number %i\n", RandomNumber());
     printf("This a random phrase taken from a text file %s", getMemePhrase());
@@ -22,7 +22,7 @@ int main()
 
     printf("spinning wheel/n");
     SpinTheWheel();
-    int scorevalue = casevalue;
+ //   int scorevalue = casevalue;
 return 0;
 
 }
