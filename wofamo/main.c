1f61ab933f2ad4166ec62a89a7a533c7dc25bd2d
relaxis <a.von.blumenthal@gmail.com>
2014-05-12 23:00:22 +0200

    for some reason this commit, RandomNumber function is borked, think it might have something to do with my main.c file code

--- wofamo/main.c
+++ wofamo/main.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include "RNG.h"
 #include "getphrase.h"
+#include "spinwheel.h"
 
 int main()
 {
     printf("This is a random number %i\n", RandomNumber());
     printf("This a random phrase taken from a text file %s", getMemePhrase());
     return 0;
+
+
+    printf("spinning wheel/n");
+    printf("the wheel landed on number %i\n", SpinTheWheel());
+
+
 }
--- wofamo/spinwheel.h
+++ wofamo/spinwheel.h
@@ -14,8 +14,21 @@ int SpinTheWheel(void){
 int wheelspinRN = RandomNumber();
 
 if (wheelspinRN == 0)
-printf("The wheel landed on");
+printf("The wheel landed on zero \n");
 return wheelspinRN;
 
+// we then do if then statements depending on what happens or we use a switch
 
+//switch(wheelstop)
+//{
+//case wheelspinRN == 0 :
+//printf("The wheel landed on/n");
+//return wheelspinRN;
+//break;
+
+//case wheelspinRN == 1 :
+//printf("The wheel landed on/n");
+//return wheelspinRN;
+//break;
+//}
 }
