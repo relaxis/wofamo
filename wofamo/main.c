b554850f43b01d8ada404d585de1e6bc1c93d2b6
marcusorly <marcusorly@gmail.com>
2014-05-12 23:35:50 +0200

    intro splash screen thingie

--- wofamo/main.c
+++ wofamo/main.c
@@ -6,7 +6,14 @@
 
 int main()
 {
-
+    printf("        Welcome, LADIES...and....GENTLEMEN!");
+    printf("             to...the one and only...");
+    printf("******************************************************");
+    printf("*                                                    *");
+    prinft("*         $|$ THE WHEEL OF FORTUNE $|$               *");
+    printf("*               €|€ THE GAME €|€                     *");
+    printf("*                                                    *");
+    printf("******************************************************");
 
     printf("This is a random number %i\n", RandomNumber());
     printf("This a random phrase taken from a text file %s", getMemePhrase());
