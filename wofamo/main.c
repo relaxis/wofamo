86726fb76a74bda9cef6eaaf2fdab2dc78c2f1aa
relaxis <a.von.blumenthal@gmail.com>
2014-05-12 23:59:07 +0200

    stuff

--- wofamo/main.c
+++ wofamo/main.c
@@ -18,6 +18,18 @@ int main()
     printf("This is a random number %i\n", RandomNumber());
     printf("This a random phrase taken from a text file %s", getMemePhrase());
 
+    int number_of_players;
+    int score_counter_1;
+    int score_counter_2;
+    int score_counter_3;
+    int turn_counter;
+    int turn_flag = 0;
+
+    while(number_of_players < 1 || number_of_players > 3){
+    printf("How many contestants are playing now?");
+    scanf("%d", &number_of_players);
+    printf("\n");
+    }
 
 
     printf("spinning wheel/n");
